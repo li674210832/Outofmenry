3d2dadbfd3a22f86d2ba21d6d8cbcb99eecdc712
平凡人生之路 <1205242073@qq.com>
2017-05-31 19:49:47 +0800

    nnn

--- app/src/main/java/otomp/ln/com/outofmenry/MainActivity.java
+++ app/src/main/java/otomp/ln/com/outofmenry/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        您奶奶
     }
 
     public void innerClass(View view){
